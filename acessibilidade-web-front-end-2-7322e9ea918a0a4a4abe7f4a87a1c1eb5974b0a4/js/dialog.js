4c63a79cdaa533f45d72273d4d5a9af65e6f19ac
João Roberto <joao.adsbrasil@gmail.com>
2025-01-12 00:55:35 -0300

    Curso finalizado!

--- acessibilidade-web-front-end-2-7322e9ea918a0a4a4abe7f4a87a1c1eb5974b0a4/js/dialog.js
+++ acessibilidade-web-front-end-2-7322e9ea918a0a4a4abe7f4a87a1c1eb5974b0a4/js/dialog.js
@@ -7,24 +7,23 @@ var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
-btnAbreDialog.style.display = 'block';
+btnAbreDialog.style.display = 'block'
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
-  document.querySelector('.dialogNewsletterlabel').focus();
+  document.querySelector('.dialogNewsletter-label').focus();
   conteudoForaDialog.inert = true;
 
-  video.removeAttribute('controls');
+  document.querySelector('video').removeAttribute('controls'); 
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');  
+  dialog.classList.remove('dialogNewsletter--aberto'); 
   conteudoForaDialog.inert = false;
   btnAbreDialog.focus();
-
-  video.setAttribute('controls', true);
+  document.querySelector('video').setAttribute('controls', 'true');
 }
 
 // Listeners
@@ -32,7 +31,16 @@ document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fec
 dialogOverlay.addEventListener('click', fechandoDialog);
 
 document.addEventListener('keyup', function(evento) {
-  if (evento.code == 27) {
-    fechandoDialog()
+  if (evento.keyCode == 27) {
+      fechandoDialog()
   }
 })
+
+
+
+
+
+
+
+
+
